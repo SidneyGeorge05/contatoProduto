e079e367221c52c7f6a140cfa7620355dbb8ce49
Sidney George <93001264+SidneyGeorge05@users.noreply.github.com>
2024-03-17 19:42:05 -0300

    Add files via upload

---
+++ bdproduto.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 17-Mar-2024 às 19:41
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bdproduto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbproduto`
+--
+
+CREATE TABLE `tbproduto` (
+  `idProduto` int(11) NOT NULL,
+  `nomeProduto` varchar(40) DEFAULT NULL,
+  `valorProduto` double DEFAULT NULL,
+  `qtdProduto` int(5) DEFAULT NULL,
+  `descProduto` varchar(5000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tbproduto`
+--
+
+INSERT INTO `tbproduto` (`idProduto`, `nomeProduto`, `valorProduto`, `qtdProduto`, `descProduto`) VALUES
+(1, 'tomate', 1000, 1000, 'tomate'),
+(2, 'laranja', 1000, 1000, 'laranja'),
+(19, 'ovo', 50, 1500, 'ovo');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `tbproduto`
+--
+ALTER TABLE `tbproduto`
+  ADD PRIMARY KEY (`idProduto`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tbproduto`
+--
+ALTER TABLE `tbproduto`
+  MODIFY `idProduto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
